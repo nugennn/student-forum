204a0e31eeb58c4ee9ce2b2c0f2b7918a8a78896
Seema Khadka <seemakhadka72@gmail.com>
2025-07-28 15:22:32 +0545

    base2

--- stdforum/forum/admin.py
+++ stdforum/forum/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Profile
 
-# Register your models here.
+class ProfileInline(admin.StackedInline):
+	model = Profile
+admin.site.unregister(Group)
+
+class UserAdmin(admin.ModelAdmin):
+	model = User
+	
+	fields = ["username"]
+	inlines = [ProfileInline]
+
+
+# Unregister initial User
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+# admin.site.register(Profile)
